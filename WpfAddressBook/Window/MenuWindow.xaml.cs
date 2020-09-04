502180cc242dda0f3b4d780e1afcab448420d49e
morimori-coder-toh <67218438+morimori-coder@users.noreply.github.com>
2020-09-04 10:43:35 +0900

    MainWindowのクラス名を変更した

--- WpfAddressBook/Window/MenuWindow.xaml.cs
+++ WpfAddressBook/Window/MenuWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace WpfAddressBook
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MenuWindow : Window
     {
-        public MainWindow()
+        public MenuWindow()
         {
             InitializeComponent();
         }
