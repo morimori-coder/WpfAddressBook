bc86cbfc3d429a4cd9fb8c0b523bf364d68fefb0
morimori-coder-toh <67218438+morimori-coder@users.noreply.github.com>
2020-09-04 11:11:51 +0900

    SearchWindowクラスを追加し、各ウィンドウクラスの継承を省略せずに記載した

---
+++ WpfAddressBook/Window/SerchWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace WpfAddressBook.Window
+{
+    /// <summary>
+    /// SerchWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SerchWindow : System.Windows.Window
+    {
+        public SerchWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
