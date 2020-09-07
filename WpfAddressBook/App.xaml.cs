c29db16282cc7f76bc2fb2df8cbe35f62595841a
morimori-coder-toh <67218438+morimori-coder@users.noreply.github.com>
2020-09-07 14:09:13 +0900

    Appクラスに例外処理を入れた

--- WpfAddressBook/App.xaml.cs
+++ WpfAddressBook/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace WpfAddressBook
 {
@@ -13,5 +14,34 @@ namespace WpfAddressBook
     /// </summary>
     public partial class App : Application
     {
+        // Startupで呼ばれるイベントハンドラ
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // アプリ開始時の処理
+
+            // アプリケーションで発生した未処理の例外の処理
+            AppDomain.CurrentDomain.UnhandledException += (o, eh) =>
+            {
+                // 例外の処理
+                var _ex = eh.ExceptionObject as Exception;
+                MessageBox.Show(_ex.ToString());
+
+                // このまま終わると動作を停止しましたというウインドウが出てしまうので
+                // 挙動を抑えるために自分で先んじで終了する
+                Environment.Exit(-1);
+            };
+        }
+
+        // DispatcherUnhandledExceptionのイベントハンドラ
+        private void Application_DispatcherUnhandledException(object sender,
+            DispatcherUnhandledExceptionEventArgs e)
+        {
+            // 未処理の例外の処理
+            Exception ex = e.Exception;
+            MessageBox.Show(ex.ToString());
+
+            // ハンドルされない例外を処理済みにするためにtrueを指定
+            e.Handled = true;
+        }
     }
 }
