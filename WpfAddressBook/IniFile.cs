c8a5aaf938883d8df547bfdc2910a7e02041cf76
morimori-coder-toh <67218438+morimori-coder@users.noreply.github.com>
2020-09-07 11:03:57 +0900

    外部ファイルからログイン情報をインプットする処理を追記した

--- WpfAddressBook/IniFile.cs
+++ WpfAddressBook/IniFile.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace WpfAddressBook
 {
     class IniFile
     {
+
+        private readonly string LOGIN_COMMAND = "loginComand";
+
+        /// <summary>
+        /// DBにログインするためのコマンド
+        /// </summary>
+        public string LoginComand { get; private set; }
+
+        public IniFile(string filePath)
+        {
+            // ファイルの存在チェック
+            // 存在しない場合はエラー終了
+            if (!System.IO.File.Exists(filePath))
+                throw new FileNotFoundException("IniFileが存在しません");
+
+            // 存在すれば内容の読み込み
+            using (var reader = new StreamReader(filePath, Encoding.UTF8))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    if (line.StartsWith(this.LOGIN_COMMAND))
+                    {
+                        this.LoginComand = line.Replace($"{this.LOGIN_COMMAND}=", "");
+                    }
+                }
+            }
+        }
     }
 }
