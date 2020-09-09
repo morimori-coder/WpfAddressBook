54792816bf852276e1a7e560fb2365762a92fe75
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2020-09-09 15:05:51 +0900

    アプリケーション実行回数をカウントするプロパティを追加

--- WpfAddressBook/AppSettings.cs
+++ WpfAddressBook/AppSettings.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WpfAddressBook
-{
-	class AppSettings
-	{
-		public int Runcount { get; private set; }
+namespace WpfAddressBook {
+	class AppSettings : ApplicationSettingsBase {
+		public AppSettings() : base(){
+		}
+		//public int RunCount { get; private set; }
+
+		[UserScopedSetting]
+		[DefaultSettingValue("0")]
+		public int RunCount {
+			get { return (int)this["RunCount"]; }
+			set { this["RunCount"] = value; }
+		}
 	}
 }
