0415d1951dc7e021cb760b4d358e4ba621460144
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2020-09-09 14:41:34 +0900

    AppSettingsクラスを追加

---
+++ WpfAddressBook/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAddressBook
+{
+	class AppSettings
+	{
+		public int Runcount { get; private set; }
+	}
+}
